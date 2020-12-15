6f69e095b86c07dc44b821ac2049384a3dc44d96
Florent TRIMOREAU <flo2533@hotmail.fr>
2020-12-15 15:59:54 +0100

    The program extract the list, order it and counts occurrences

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,22 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-
+/**
+ * This program allows you to retrieve a list of String from a file, order them
+ * alphabetically, counts the number of occurrences, and then deliver the output
+ * in a result file.
+ * 
+ * @author Draflo
+ *
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+		ReadSymptomDataFromFile ListSymptom = new ReadSymptomDataFromFile("symptoms.txt");
+		System.out.println(ListSymptom.GetSymptoms());
+		// List of symptoms as it is in the file
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		ListNumberedOrdered ReadyToPrint = new ListNumberedOrdered(ListSymptom.GetSymptoms());
+		System.out.println(ReadyToPrint.listnumberedordered());
+		// List of symptoms ordered and with occurrences
 	}
 }
