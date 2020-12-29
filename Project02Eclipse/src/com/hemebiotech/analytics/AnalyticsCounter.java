481de08cc20465b8a28c6743140c4a1303e7612e
Florent TRIMOREAU <flo2533@hotmail.fr>
2020-12-29 14:29:43 +0100

    Correction merge

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -11,44 +11,12 @@ package com.hemebiotech.analytics;
 public class AnalyticsCounter {
 
 	public static void main(String args[]) throws Exception {
-<<<<<<< HEAD
-		try (// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"))) {
-			String line = reader.readLine();
-=======
+
 		ISymptomReader symptomReader = new ReadSymptomDataFromFile("symptoms.txt");
 		//Créer une list fixe
 		System.out.println(symptomReader.GetSymptoms());
 		// List of symptoms as it is in the file
->>>>>>> refs/heads/release/V1_GetSymptom_Count_Order
 
-<<<<<<< HEAD
-			
-			while (line != null) {
-				System.out.println("symptom from file: " + line);
-				if (line.equals("headache")) {
-					headacheCount++;
-					System.out.println("number of headaches: " + headacheCount);
-				}
-				else if (line.equals("rash")) {
-					rashCount++;
-					System.out.println("number of rash: " + rashCount);
-				}
-				else if (line.contains("pupils")) {
-					pupilCount++;
-					System.out.println("number of pupils: " + pupilCount);
-				}
-
-				line = reader.readLine();	// get another symptom
-			}
-		}
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-=======
 		ListNumberedOrdered ReadyToPrint = new ListNumberedOrdered(symptomReader.GetSymptoms());
 		//Créer une map fixe
 		System.out.println(ReadyToPrint.listnumberedordered());
@@ -56,6 +24,6 @@ public class AnalyticsCounter {
 		
 		ResultsWriter Results = new ResultsWriter(ReadyToPrint);
 		Results.writeToFile();
->>>>>>> refs/heads/release/V1_GetSymptom_Count_Order
+
 	}
 }
