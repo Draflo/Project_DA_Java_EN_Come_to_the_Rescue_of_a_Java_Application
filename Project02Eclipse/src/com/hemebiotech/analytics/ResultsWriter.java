600383a6fa01cc5c00d8914c23b7fb9e3d22e013
Florent TRIMOREAU <flo2533@hotmail.fr>
2021-01-04 17:28:36 +0100

    Squashed commit of the following:
    commit 57a0c3d2b0ce91fc95ebce15704e69b3c05fee53
    Author: Florent TRIMOREAU <flo2533@hotmail.fr>
    Date:   Mon Jan 04 17:28:11 2021 +0100
    
    	Javadoc created, code cleaned, nearly ready

--- Project02Eclipse/src/com/hemebiotech/analytics/ResultsWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ResultsWriter.java
@@ -7,35 +7,42 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class ResultsWriter {
+public class ResultsWriter implements Writer {
 
 	private Map<String, Long> filepath;
-	
+
 	/**
 	 * 
-	 * @param filepath a full or partial path to the Map with our symptoms and their occurrences
+	 * @param filepath a full or partial path to the Map with our symptoms and their
+	 *                 occurrences
 	 */
-	
+
 	public ResultsWriter(Map<String, Long> symptomcountorder) {
 		this.filepath = symptomcountorder;
 	}
 
-	public void writeToFile() {
-		
+	@Override
+	public void writetofile() throws IOException {
+
 		TreeMap<String, Long> map = new TreeMap<String, Long>();
 		map.putAll(filepath);
+		/**
+		 * Create a Map to get all the data of filepath so it can be written in the new
+		 * file
+		 */
 		File file = new File("result.out");
+		BufferedWriter bf = new BufferedWriter(new FileWriter(file));
 		try {
 
-			BufferedWriter bf = new BufferedWriter(new FileWriter(file));
 			for (Map.Entry<String, Long> entry : map.entrySet()) {
 				bf.write(entry.getKey() + " = " + entry.getValue());
 				bf.newLine();
 				bf.flush();
 			}
 			bf.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+		} finally {
+			if (bf != null)
+				bf.close();
 		}
 
 	}
